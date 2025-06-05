691bcc51d59c81e4aff90125a581900bd6976d8c
Nikola Gaic <nikolagaic@yahoo.com>
2025-06-05 17:18:39 +0200

    Add project files.

---
+++ Movies.API/Controllers/MoviesController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Movies.API.Mapping;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+
+namespace Movies.API.Controllers
+{
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        [HttpPost(ApiEndpoints.Movies.Create)]
+        public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+        {
+            var movie = request.MapToMovie();
+
+            await _movieRepository.CreateAsync(movie);
+
+            var response = new MovieResponse
+            {
+                Id = movie.Id,
+                Title = movie.Title,
+                YearOfRelease = movie.YearOfRelease,
+                Genres = movie.Genres
+            };
+
+            return CreatedAtAction(nameof(Get), new { id = response.Id }, response);
+        }
+
+
+        [HttpGet(ApiEndpoints.Movies.Get)]
+        public async Task<IActionResult> Get([FromRoute] Guid id)
+        {
+            var movie = await _movieRepository.GetByIdAsync(id);
+            if (movie is null)
+            {
+                return NotFound();
+            }
+
+            var response = movie.MapToResponse();
+
+            return Ok(response);
+        }
+
+        [HttpGet(ApiEndpoints.Movies.GetAll)]
+        public async Task<IActionResult> GetAll()
+        {
+            var movies = await _movieRepository.GetAllAsync();
+            var response = movies.MapToResponse();
+            return Ok(response);
+        }
+
+
+        [HttpPut(ApiEndpoints.Movies.Update)]
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateMovieRequest request)
+        {
+            var movie = request.MapToMovie(id);
+            var updatedMovie = await _movieRepository.UpdateAsync(movie);
+
+            if (!updatedMovie)
+            {
+                return NotFound();
+            }
+
+            var response = movie.MapToResponse();
+            return Ok(response);
+        }
+
+        [HttpDelete(ApiEndpoints.Movies.Delete)]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var deletedMovie = await _movieRepository.DeleteByIdAsync(id);
+            if (!deletedMovie)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+    }
+}
---
+++ Movies.Application/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Application.Repositories;
+
+namespace Movies.Application
+
+{
+    public static class ApplicationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            // Register application services here
+            services.AddSingleton<IMovieRepository, MovieRepository>();
+            return services;
+        }
+
+    }
+}
