0323cdab8800291ed1c62fc0e850199b2193122f
Nikola Gaic <nikolagaic@yahoo.com>
2025-06-13 18:27:48 +0200

    Integrated auth in Swagger

--- Movies.API/Swagger/ConfigureSwaggerOptions.cs
+++ Movies.API/Swagger/ConfigureSwaggerOptions.cs
@@ -26,6 +26,31 @@ namespace Movies.API.Swagger
                     Version = description.ApiVersion.ToString(),
                 });
             }
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
         }
     }
 }
